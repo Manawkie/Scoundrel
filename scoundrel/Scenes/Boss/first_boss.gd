8a957c29b44b61f7ad429f4b4802a51e3e323026
Manawkie <samenianjoshua@gmail.com>
2025-10-14 03:38:11 +0800

    First boss anims

---
+++ scoundrel/Scenes/Boss/first_boss.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var anim_player = $AnimationPlayer
+@onready var player = get_parent().get_node("Player")
+var state_machine : AnimationNodeStateMachinePlayback
+
+enum BossState {FULL_HEALTH, HALF_HEALTH, QUARTER_HEALTH}
+
+
+# --- BOSS VARIABLES ---
+var hp = 100
+
+func _ready() -> void:
+	anim_tree.active = true
+	state_machine = anim_tree.get("parameters/playback")
+	
+	
+
+
+func _process(_delta: float) -> void:
+	#sprite_direction()
+	pass
+	
+
+
+
+func sprite_direction():
+	if player.global_position.x > global_position.x:
+		# Player is on the right
+		sprite.flip_h = false
+	else:
+		# Player is on the left
+		sprite.flip_h = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func decrease_hp(damage):
+	hp -= damage
+	
